ac5a30023003e97b2b49968d8505dc59dd72c728
dancewhale <542727233@qq.com>
2024-09-18 14:32:01 +0800

    Feat: add elisp org setting file.

--- custom.el
+++ custom.el
@@ -4,8 +4,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auto-save-list-file-prefix "~/.emacs.d/.local/cache/auto-save-list/.saves-")
- '(custom-safe-themes
-   '("1f292969fc19ba45fbc6542ed54e58ab5ad3dbe41b70d8cb2d1f85c22d07e518" default))
  '(org-gtd-directory "/home/whale/Dropbox/roam/gtd"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
