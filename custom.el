a7d8d5ca395d9291522e03cc292a6035a4adb9f5
caoyuanzhi <542727233@qq.com>
2024-12-09 20:39:41 +0800

    从hamacs 移植elisp  org  evil配置

--- custom.el
+++ custom.el
@@ -4,7 +4,27 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auto-save-list-file-prefix "~/.emacs.d/.local/cache/auto-save-list/.saves-")
- '(org-gtd-directory "/home/whale/Dropbox/roam/gtd"))
+ '(ignored-local-variable-values
+   '((system-time-locale . "C")
+     (jinx-local-words . "Braganza Graphviz Malorana’s Proselint Somers Textlint Writegood flycheck flyspell fontlock")
+     (jinx-local-words . "parseable")
+     (eval add-hook 'after-save-hook #'org-babel-tangle t t)
+     (elisp-lint-indent-specs
+      (describe . 1)
+      (it . 1)
+      (thread-first . 0)
+      (cl-flet . 1)
+      (cl-flet* . 1)
+      (org-element-map . defun)
+      (org-roam-dolist-with-progress . 2)
+      (org-roam-with-temp-buffer . 1)
+      (org-with-point-at . 1)
+      (magit-insert-section . defun)
+      (magit-section-case . 0)
+      (org-roam-with-file . 2))
+     (elisp-lint-ignored-validators "byte-compile" "package-lint")))
+ '(org-gtd-directory "/home/whale/Dropbox/roam/gtd")
+ '(rime-emacs-module-header-root "/opt/homebrew/include"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
