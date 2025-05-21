81581dc822695fc2d6c20588be74e7c2e92ed9fb
dancewhale <542727233@qq.com>
2025-05-21 16:34:17 +0800

    Fix custom setting for agenda.

--- custom.el
+++ custom.el
@@ -25,10 +25,6 @@
      (elisp-lint-ignored-validators "byte-compile" "package-lint")))
  '(lsp-go-directory-filters ["-/opt/homebrew/"])
  '(lsp-go-library-directories '("/opt/homebrew/Cellar/go/"))
- '(org-agenda-files
-   '("/Users/whale/Dropbox/roam/gtd/tasks.org" "/Users/whale/Dropbox/roam/gtd/notes.org"))
- '(org-gtd-directory "/home/whale/Dropbox/roam/gtd")
- '(rime-emacs-module-header-root "/opt/homebrew/include")
  '(straight-disable-compile t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
