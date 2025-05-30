878d945e45578ade86270942ddcbf830ba5ada5a
dancewhale <542727233@qq.com>
2025-05-30 16:08:32 +0800

    Add super-agenda package and config.
    Setting org-journal time format.

--- custom.el
+++ custom.el
@@ -6,25 +6,26 @@
  '(auto-save-list-file-prefix "~/.emacs.d/.local/cache/auto-save-list/.saves-")
  '(ignored-local-variable-values
    '((system-time-locale . "C")
-     (jinx-local-words . "Braganza Graphviz Malorana’s Proselint Somers Textlint Writegood flycheck flyspell fontlock")
+     (jinx-local-words
+      . "Braganza Graphviz Malorana’s Proselint Somers Textlint Writegood flycheck flyspell fontlock")
      (jinx-local-words . "parseable")
      (eval add-hook 'after-save-hook #'org-babel-tangle t t)
-     (elisp-lint-indent-specs
-      (describe . 1)
-      (it . 1)
-      (thread-first . 0)
-      (cl-flet . 1)
-      (cl-flet* . 1)
-      (org-element-map . defun)
-      (org-roam-dolist-with-progress . 2)
-      (org-roam-with-temp-buffer . 1)
-      (org-with-point-at . 1)
-      (magit-insert-section . defun)
-      (magit-section-case . 0)
-      (org-roam-with-file . 2))
+     (elisp-lint-indent-specs (describe . 1) (it . 1)
+			      (thread-first . 0) (cl-flet . 1)
+			      (cl-flet* . 1) (org-element-map . defun)
+			      (org-roam-dolist-with-progress . 2)
+			      (org-roam-with-temp-buffer . 1)
+			      (org-with-point-at . 1)
+			      (magit-insert-section . defun)
+			      (magit-section-case . 0)
+			      (org-roam-with-file . 2))
      (elisp-lint-ignored-validators "byte-compile" "package-lint")))
  '(lsp-go-directory-filters ["-/opt/homebrew/"])
  '(lsp-go-library-directories '("/opt/homebrew/Cellar/go/"))
+ '(org-agenda-files
+   '("/home/whale/Dropbox/roam/gtd/inbox.org"
+     "/home/whale/Dropbox/roam/gtd/org-gtd-tasks.org"
+     "/home/whale/Dropbox/worklog/2025-05-01.org"))
  '(straight-disable-compile t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
